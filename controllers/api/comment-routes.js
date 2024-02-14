75d0cdb0b71cca75ef08cfeb2589bab48d9c56e2
A04pr <areed041704@gmail.com>
2024-02-14 16:21:18 -0700

    controllers

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,63 @@
+const router = require('express').Router();
+const { Comment, User, Post } = require('../models');
+
+router.get('/comments', async (req, res) => {
+  try {
+    const commentData = await Comment.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Post,
+          attributes: ['title'],
+        },
+      ],
+    });
+
+    const comments = commentData.map((comment) => comment.get({ plain: true }));
+
+    res.render('comments', { comments });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post('/comments', async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      comment_text: req.body.comment_text,
+      user_id: req.session.user_id, 
+      post_id: req.body.post_id,
+    });
+
+    res.status(201).json(newComment);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/comments/:id', async (req, res) => {
+  try {
+    const deletedComment = await Comment.destroy({
+      where: {
+        comment_id: req.params.id,
+      },
+    });
+
+    if (!deletedComment) {
+      res.status(404).json({ message: 'No comment found with this id' });
+      return;
+    }
+
+    res.status(200).json(deletedComment);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,14 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const postRoutes = require('./post-routes.js'); 
+const userRoutes = require('./user-routes.js'); 
+const commentRoutes = require('./comment-routes.js'); 
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/posts', postRoutes); 
+router.use('/users', userRoutes); 
+router.use('/comments', commentRoutes); 
 
 module.exports = router;
