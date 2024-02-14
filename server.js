e65557ae011ad5833104a9af9e9f96afb48c78a5
A04pr <areed041704@gmail.com>
2024-02-14 07:40:14 -0700

    server setup

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'secretkey',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.use(routes);
+
+db.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+});
